0aed91a02b369593e74c1bc23635174b9ff2b421
Afonso Vidal <afonsoxvp@hotmail.com>
2023-04-22 10:45:32 +0100

    Melhoria da funcionalidade da soma

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n =0;
-		int n1=3;
-		int soma;
-		soma = n+ n1;
-		System.out.println("soma : " + soma);
+		int n1 =0;
+		int n2=3;
+		int soma = Soma(n1,n2);
+		System.out.println("soma : " + soma);		
 	}
 
+	public static int Soma(int n1, int n2) {//absdlbasd
+		return n1+n2;
+	}
 }
