1776537f2643c71497ce5801ab0259657ab68016
4444-prog <duartedias12314@gmail.com>
2023-04-22 10:23:57 +0100

    soma

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n =0;
+		int n1=3;
+		int soma;
+		soma = n+ n1;
+		System.out.println("soma : " + soma);
 	}
 
 }
